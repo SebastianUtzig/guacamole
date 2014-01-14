80ae45a775b12967ad0fa717a483ac0eb696c9af
Sebastian Utzig <sebastian.utzig@uni-weimar.de>
2014-01-14 18:57:12 +0100

    first prototype of group and ungroup physicalgeometries...not tested yet...no rigidbody callbacks yet

--- plugins/guacamole-physics/include/PhysicalGeometryNode.hpp
+++ plugins/guacamole-physics/include/PhysicalGeometryNode.hpp
@@ -11,6 +11,7 @@
 
 // external headers
 #include <string>
+#include <utility>
 
 namespace gua{
 	
@@ -19,31 +20,41 @@ class GUA_DLL PhysicalGeometryNode: public GeometryNode{
 
   public:
 
-  	struct Configuration {
+  	/*struct Configuration {
       GUA_ADD_PROPERTY(std::string,     geometry,   "gua_default_geometry");
       GUA_ADD_PROPERTY(std::string,     material,   "gua_default_material");
-    };
+    };*/
 
-    Configuration data;
+    //Configuration data;
 
     PhysicalGeometryNode() {};
 
   	PhysicalGeometryNode(std::string const& name,
+                 physics::Physics* physics,
                  GeometryNode::Configuration const& configuration = GeometryNode::Configuration(),
                  math::mat4 const& transform = math::mat4::identity(),
-                 float mass = 0,
-                 float _friction = 0,
-                 float _restitution = 0,
+                 float mass = 0.5f,
+                 float friction = 2.9f,
+                 float restitution = 0.3f,
                  std::shared_ptr<physics::CollisionShapeNode> cs = nullptr);
 
-  	bool 	make_collidable(bool,physics::Physics*);
+  	bool 	make_collidable(bool);
   	bool	is_collidable() const;
+
+    void  set_world_transform(math::mat4 const&);
+    std::shared_ptr<physics::CollisionShapeNode>  get_collision_shape() const;
 
 
 
   private:
 
-  	std::shared_ptr<physics::RigidBodyNode>      rigid_body_;
+    void collect_collision_shapes(std::shared_ptr<Node>,std::list<std::pair<std::shared_ptr<Node>,math::mat4>>&)const;
+    void warn_parent_physics(std::shared_ptr<Node>)const;
+    void update_physics_structure();
+
+  	
+    physics::Physics*                            physics_;
+    std::shared_ptr<physics::RigidBodyNode>      rigid_body_;
   	std::shared_ptr<physics::CollisionShapeNode> collision_shape_;
   	float				                                 mass_;
   	float				                                 friction_;
--- plugins/guacamole-physics/src/PhysicalGeometryNode.cpp
+++ plugins/guacamole-physics/src/PhysicalGeometryNode.cpp
@@ -6,6 +6,7 @@ namespace gua{
 
 
 PhysicalGeometryNode::PhysicalGeometryNode(std::string const& name,
+				 physics::Physics* physics,
                  GeometryNode::Configuration const& configuration,
                  math::mat4 const& transform,
                  float mass,
@@ -17,32 +18,50 @@ PhysicalGeometryNode::PhysicalGeometryNode(std::string const& name,
 			mass_(mass),
 			friction_(friction),
 			restitution_(restitution),
-			collision_shape_(cs)
+			collision_shape_(cs),
+			physics_(physics)
 		{}
 
 
 
 bool
-PhysicalGeometryNode::make_collidable(bool make_collidable,physics::Physics* physics){
+PhysicalGeometryNode::make_collidable(bool b_make_collidable){
 
-	if(make_collidable and rigid_body_==nullptr){
+	if(b_make_collidable){
 
-		rigid_body_= std::shared_ptr<physics::RigidBodyNode>(new physics::RigidBodyNode(Node::get_name()+"_rb_",mass_,friction_,restitution_,Node::get_world_transform()));
+		if(rigid_body_==nullptr){
 
-		if (collision_shape_){
-			rigid_body_->add_child(collision_shape_);
+			if (collision_shape_ == nullptr){
+				//TODO compute collision shape automatically here!
+				std::cout<<"Missing CollisionShape in make_collidable()!!!"<<std::endl;
+				return false;
+			}
+
+			rigid_body_= std::shared_ptr<physics::RigidBodyNode>(new physics::RigidBodyNode(get_name()+"_rb_",mass_,friction_,restitution_,get_world_transform()));
+
+			//rigid_body_->add_child(collision_shape_);
+
+			auto collision_shapes = std::list<std::pair<std::shared_ptr<Node>,math::mat4>>();
+
+			collect_collision_shapes(std::shared_ptr<Node>(this),collision_shapes);
+
+			for(auto cs : collision_shapes){
+				cs.first->set_transform(scm::math::inverse(get_world_transform()) * cs.second);
+				rigid_body_->add_child(cs.first);
+			}
+
+			physics_->add_rigid_body(rigid_body_);
+			warn_parent_physics(get_parent_shared());
 		}
 		else{
-			//TODO compute collision shape automatically here!
-			std::cout<<"Missing CollisionShape in make_collidable()!!!"<<std::endl;
-			return false;
+			make_collidable(false);
+			return make_collidable(true);
 		}
-
-		physics->add_rigid_body(rigid_body_);
 	}
 	else{
-		physics->remove_rigid_body(rigid_body_);
+		physics_->remove_rigid_body(rigid_body_);
 		rigid_body_ = nullptr;
+		warn_parent_physics(get_parent_shared());
 	}
 
 	return true;
@@ -56,6 +75,65 @@ PhysicalGeometryNode::is_collidable()const{
 		return true;
 	}
 	return false;
+}
+
+
+void
+PhysicalGeometryNode::set_world_transform(math::mat4 const& transform){
+	auto parent = get_parent();
+    set_transform(scm::math::inverse(parent->get_world_transform())* transform);
+}
+
+
+
+std::shared_ptr<physics::CollisionShapeNode>
+PhysicalGeometryNode::get_collision_shape() const{
+	if (collision_shape_){return collision_shape_;}
+	else {return nullptr;}
+}
+
+
+void
+PhysicalGeometryNode::collect_collision_shapes(std::shared_ptr<Node> node,std::list<std::pair<std::shared_ptr<Node>,math::mat4>>& collision_shapes)const{
+	if(dynamic_cast<PhysicalGeometryNode*>(node.get())){
+		auto phys_node = dynamic_cast<PhysicalGeometryNode*>(node.get());
+		if(!phys_node -> is_collidable()){
+			for(auto child : phys_node->get_children()){
+				collect_collision_shapes(child,collision_shapes);
+			}
+			if(phys_node->get_collision_shape()){
+				collision_shapes.push_back(std::make_pair(phys_node->get_collision_shape(),phys_node->get_world_transform()));
+			}
+		}
+	}
+	else{
+		for(auto child : node->get_children()){
+			collect_collision_shapes(child,collision_shapes);
+		}
+	}
+}
+
+void
+PhysicalGeometryNode::warn_parent_physics(std::shared_ptr<Node> parent)const{
+	if(dynamic_cast<PhysicalGeometryNode*>(parent.get())){
+		auto phys_node = dynamic_cast<PhysicalGeometryNode*>(parent.get());
+		if(phys_node->is_collidable()){
+			phys_node->update_physics_structure();
+		}
+		else if(parent->get_parent_shared()){
+			warn_parent_physics(parent->get_parent_shared());
+		}
+	}
+	else if(parent->get_parent_shared()){
+		warn_parent_physics(parent->get_parent_shared());
+	}
+
+}
+
+void
+PhysicalGeometryNode::update_physics_structure(){
+	make_collidable(false);
+	make_collidable(true);
 }
 
 
